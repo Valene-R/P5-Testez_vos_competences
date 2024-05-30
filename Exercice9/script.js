26b030338c1f8ca698345b9b49d9e72f4cd0decd
Valène Renaud <valene.renaud@gmail.com>
2024-05-30 17:57:51 +0200

    feat(exercice9): implement 'checkAge' function to classify age groups and screenshot of test results

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,11 @@
-// votre code ici
+function checkAge(age) {
+    if (age < 18) {
+        return "Vous êtes mineur.";
+    } else if (age >= 18 && age < 65) { // Le test n'inclut pas les personnes de 65 ans (<= 65) comme indiqué dans les instructions
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes senior.";
+    }
+}
 
 export default checkAge;
