7d94d1e955f9d9efdcb2fae2434802628b065079
Valène Renaud <valene.renaud@gmail.com>
2024-05-30 18:12:20 +0200

    feat(exercice10): implement 'printNumbers' function to return string of numbers from 1 to n and add screenshot of test results

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,9 @@
-//votre code ici
+function printNumbers(n) {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+        result += i + " ";  // Ajoute chaque nombre à la chaîne suivie d'un espace
+    }
+    return result.trim();  // Supprime l'espace supplémentaire à la fin
+}
 
 export default printNumbers
