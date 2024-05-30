e269aa84961fbef2dfc2b0ce74ee3c320e3fd7d1
Valène Renaud <valene.renaud@gmail.com>
2024-05-30 11:49:54 +0200

    feat(exercice8): implement 'add' function to sum two numbers with type conversion and screenshot of test results

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,9 @@
-//Votre code ici
+function add(num1, num2) {
+    // Convertit les entrées en nombres flottants en cas de chaînes de caractères 
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    return number1 + number2;
+}
 
  export default add;
